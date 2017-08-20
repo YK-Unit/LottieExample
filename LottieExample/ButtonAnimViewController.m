54276d3223f81dd7c75a0048f742a383ffbbd9c8
York <yorkzhang520@gmail.com>
2017-08-20 17:36:12 +0800

    增加Play-Pause按钮示例，以及优化按钮的状态切换

--- LottieExample/ButtonAnimViewController.m
+++ LottieExample/ButtonAnimViewController.m
@@ -17,6 +17,9 @@
 @property (nonatomic,strong) LOTAnimationView *editButton;
 @property (nonatomic,assign) BOOL isEditing;
 
+@property (nonatomic,strong) LOTAnimationView *playButton;
+@property (nonatomic,assign) BOOL isPlaying;
+
 @end
 
 @implementation ButtonAnimViewController
@@ -35,7 +38,7 @@
     [self.view addSubview:self.switchButton];
     [self.switchButton setValue:[UIColor orangeColor] forKeypath:@"Background 2.Shape 1.Fill 1.Color" atFrame:@(0)];
     [self.switchButton setValue:[UIColor blueColor] forKeypath:@"Background 2.Shape 1.Fill 1.Color" atFrame:@(13)];
-
+    [self.switchButton logHierarchyKeypaths];
 
     self.editButton = [LOTAnimationView animationNamed:@"Edit"];
     self.editButton.contentMode = UIViewContentModeScaleAspectFit;
@@ -44,7 +47,16 @@
     UITapGestureRecognizer *editButtonTapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(editButtonClicked)];
     [self.editButton addGestureRecognizer:editButtonTapGR];
     [self.view addSubview:self.editButton];
+    [self.editButton setProgressWithFrame:@(54)];
 
+    self.playButton = [LOTAnimationView animationNamed:@"Play-Pause"];
+    self.playButton.contentMode = UIViewContentModeScaleAspectFit;
+    //playButton的width和height从AE文件中获得
+    self.playButton.frame = CGRectMake(230, 100, 160/2, 160/2);
+    UITapGestureRecognizer *playButtonTapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(playButtonClicked)];
+    [self.playButton addGestureRecognizer:playButtonTapGR];
+    [self.view addSubview:self.playButton];
+    [self.playButton setProgressWithFrame:@(50)];
 
 
     UIButton *giftButton = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -75,16 +87,43 @@
 
 - (void)editButtonClicked {
     if (self.isEditing) {
-        [self.editButton playFromFrame:@(105) toFrame:@(54) withCompletion:nil];
+        [self.editButton setProgressWithFrame:@(166)];
+        [self.editButton playFromFrame:@(166) toFrame:@(218) withCompletion:nil];
 
         NSLog(@"结束编辑...");
     } else {
+        [self.editButton setProgressWithFrame:@(54)];
         [self.editButton playFromFrame:@(54) toFrame:@(105) withCompletion:nil];
 
         NSLog(@"开始编辑...");
     }
 
     self.isEditing = !self.isEditing;
+}
+
+- (void)playButtonClicked {
+    __weak typeof(self) weakSelf = self;
+
+
+    self.playButton.loopAnimation = NO;
+    self.playButton.autoReverseAnimation = NO;
+
+    if (self.isPlaying) {
+        [self.playButton setProgressWithFrame:@(180)];
+        [self.playButton playFromFrame:@(180) toFrame:@(213) withCompletion:^(BOOL animationFinished) {
+        }];
+
+    } else {
+        [self.playButton setProgressWithFrame:@(50)];
+        [self.playButton playFromFrame:@(50) toFrame:@(90) withCompletion:^(BOOL animationFinished) {
+            weakSelf.playButton.loopAnimation = YES;
+            weakSelf.playButton.autoReverseAnimation = YES;
+            [weakSelf.playButton playFromFrame:@(90) toFrame:@(180) withCompletion:^(BOOL animationFinished) {
+            }];
+        }];
+    }
+
+    self.isPlaying = !self.isPlaying;
 }
 
 - (void)giftButtonClicked:(UIButton *)button {
